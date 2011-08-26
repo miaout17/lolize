76a06aaea395c89525cd1100a5251955b286707b
BigCat <miaout17@gmail.com>
2011-08-26 23:42:09 +0800

    color based on lines

--- lib/lolize.rb
+++ lib/lolize.rb
@@ -4,5 +4,3 @@ require 'lolize/colorizer'
 Lolize::Colorizer.lolize($stdout)
 Lolize::Colorizer.lolize($stderr)
 
-# 100.times { puts "A"*100 }
-# puts "a"
--- lib/lolize/colorizer.rb
+++ lib/lolize/colorizer.rb
@@ -16,16 +16,18 @@ module Lolize
 
     def initialize
       @freq = 0.3
-      @i = 0
+      @spread = 8.0
+      @line_color = 0
+      @color = 0
 
       @state = :normal
     end
 
     def rainbow
-      red = Math.sin(@freq*@i + 0) * 127 + 128
-      green = Math.sin(@freq*@i + 2*Math::PI/3) * 127 + 128
-      blue  = Math.sin(@freq*@i + 4*Math::PI/3) * 127 + 128
-      @i += 1
+      red = Math.sin(@freq*@color + 0) * 127 + 128
+      green = Math.sin(@freq*@color + 2*Math::PI/3) * 127 + 128
+      blue  = Math.sin(@freq*@color + 4*Math::PI/3) * 127 + 128
+      @color += 1/@spread
       "#%02X%02X%02X" % [ red, green, blue ]
     end
 
@@ -35,13 +37,21 @@ module Lolize
         when :normal
           if c=="\e"
             @state = :ansi
+          elsif c=="\n"
+            @line_color += 1
+            @color = @line_color
+            raw_write(c)
           else
-            $stdout.raw_write Paint[c, rainbow]
+            raw_write(Paint[c, rainbow])
           end
         when :ansi
           @state = :normal if c=='m'
         end
       end
+    end
+
+    def raw_write(s)
+      $stdout.raw_write(s)
     end
   end
 end
