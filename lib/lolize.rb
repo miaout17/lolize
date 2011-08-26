618afeacb7d017f9ab4f75572e0477f8f217438e
BigCat <miaout17@gmail.com>
2011-08-26 10:27:54 +0800

    workable dirty code

--- lib/lolize.rb
+++ lib/lolize.rb
@@ -1,3 +1,38 @@
+require 'lolcat'
+
 module Lolize
-  # Your code goes here...
+  def self.write(s)
+    @opts ||= {
+      :animate => false,
+      :duration => 12,
+      :os => 0,
+      :speed => 20,
+      :spread => 8.0,
+      :freq => 0.3
+    }
+    Lol.println(s, {}, @opts)
+  end
+  def self.lolize(output)
+    class << output
+      alias :raw_write :write
+      def write(s)
+        Lol.println(s)
+      end
+    end
+  end
 end
+
+module Lol
+  # def self.puts
+  # end
+  # def self.print
+  # end
+end
+
+# Lolize.lolize($stdout)
+# Lolize.lolize($stderr)
+
+Lolize.write "aa"*1000
+
+exit 0
+
